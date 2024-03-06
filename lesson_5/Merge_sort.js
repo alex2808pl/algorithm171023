61358dfb691dfe17a19d499e718366d31f87bbc0
Aleksandr Plakushchyi <alex2808pl@gmail.com>
2024-03-06 13:12:53 +0200

    Merge_sort.js

---
+++ lesson_5/Merge_sort.js
@@ -0,0 +1,33 @@
+/** Сортировка слиянием */
+
+function mergeSort(arr) {
+    if (arr.length === 1) {
+        return arr;
+    }
+    let mid = Math.ceil(arr.length / 2);
+    let leftArr = arr.slice(0, mid);
+    let rightArr = arr.slice(mid);
+    console.log(leftArr + " == "+rightArr);
+    return merge(mergeSort(leftArr), mergeSort(rightArr));
+}
+
+function merge(leftArr, rightArr) {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+        if (leftArr[leftIndex] < rightArr[rightIndex]) {
+            result.push(leftArr[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(rightArr[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    return  result.concat(leftArr.slice(leftIndex)).concat(rightArr.slice(rightIndex));
+    //console.log(leftArr + " = "+rightArr+" ===> "+result);
+    // return result;
+}
+
+console.log(mergeSort([38, 27, 43, 3, 9, 82, 10]));
